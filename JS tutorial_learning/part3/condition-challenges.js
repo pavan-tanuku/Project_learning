d5600d8d24973733a16e54ac835003f17078ff22
pavan <pavan.tanuku@sasi.ac.in>
2025-09-24 02:08:46 +0530

    adding part3 of js tutorial

---
+++ JS tutorial_learning/part3/condition-challenges.js
@@ -0,0 +1,55 @@
+// checking if a number is greater than another number:
+/*let num1 = 5;
+let num2 = 8;
+console.log("I am regular upper code");
+
+if (num1 > num2) {
+    console.log("num 1 is greater than num 2");
+} else {
+    console.log("Nope, num1 is NOT greater");
+}
+
+console.log("I am regular lower code");
+*/
+
+// checking if a string is equal to another string:
+/*let username = "chai";
+let anotherUsername = "chai";
+
+if (username != anotherUsername) {
+    console.log("Pick another username.");
+} else {
+    console.log("You can use this username.");
+}
+*/
+
+// checking if a variable is a number or not:
+/*let score = '44';
+
+if (typeof score === 'number') {
+    console.log("Yep, this is a number");
+} else {
+    console.log("No, that is not a number");
+}
+*/
+
+// check if a boolean value is true or not
+let isTeaReady = false;
+
+if (isTeaReady) {
+    console.log("Tea is Ready");
+} else {
+    // console.log("Tea is NOT Ready");
+}
+
+// check if an array is empty or not
+let item = [""];
+
+console.log(item.length);
+if (item.length == 0) {
+    console.log("Array is empty");
+    
+} else {
+    console.log("Array is NOT empty");
+    
+}
